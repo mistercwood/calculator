20d9d503712cc8b106a7945810ebdc4b65ac7095
Chris <mistercwood@gmail.com>
2021-06-27 18:16:07 +1000

    Thorough rework of calculation logic upon keypresses, still some bugs to resolve but primary functionality working.

--- script.js
+++ script.js
@@ -1,57 +1,105 @@
 let displayValue = document.getElementById('display').innerHTML;
-let runningTotal = [];
+let runningTotal1 = [];
+let runningTotal2 = [];
+let num1;
+let num2;
+let currentOperator;
 
-function updateCount(x) {
+function updateNumber(x) {
     document.getElementById('display').innerHTML += x;
-    if (typeof x === 'number' && runningTotal.length < 2) {
-        runningTotal.push(x);
-        let tempNum = runningTotal.join('');
-        runningTotal = [tempNum];
+    if (num1 && currentOperator) {
+        runningTotal2.push(x);
+        num2 = runningTotal2.join('');
     }
     else {
-        runningTotal.push(x);
+        runningTotal1.push(x)
+        num1 = runningTotal1.join('');
+    }
+}
+
+function updateOperator(x) {
+    if (num1 && num2 && currentOperator) {
+        num1 = runningTotal1.join('');
+        num2 = runningTotal2.join('');
+        result = calculate(num1, currentOperator, num2);
+        document.getElementById('display').innerHTML = result;
+        document.getElementById('display').innerHTML += x;
+        num1 = result;
+        num2 = '';
+        currentOperator = x;
+    }
+    else if (num1 && currentOperator) {
+        currentOperator = x;
+        document.getElementById('display').innerHTML += x;
+    }
+    else if (num1) {
+        currentOperator = x;
+        document.getElementById('display').innerHTML += x;
+    }
+    else {
+        console.log('something not right?');
+    }
+}
+
+function equals() {
+    if (num1 && num2 && currentOperator) {
+        num1 = parseFloat(runningTotal1.join(''));
+        num2 = parseFloat(runningTotal2.join(''));
+        result = calculate(num1, currentOperator, num2);
+        document.getElementById('display').innerHTML = result;
+        num1 = result;
+        num2 = '';
+        runningTotal1 = [num1];
+        runningTotal2 = [];
+    }
+    else {
+        return;
     }
 }
 
 function clearAll() {
     document.getElementById('display').innerHTML = '';
-    runningTotal = [];
+    runningTotal1 = [];
+    runningTotal2 = [];
+    num1 = '';
+    num2 = '';
+    currentOperator = '';
 }
 
-function backSpace() {
-    runningTotal.splice(-1);
-    document.getElementById('display').innerHTML = runningTotal.join('');
-}
+// function backSpace() {
+//     runningTotal.splice(-1);
+//     document.getElementById('display').innerHTML = runningTotal.join('');
+// }
 
 const oneButton = document.querySelector('#one');
-oneButton.addEventListener('click', () => updateCount(1));
+oneButton.addEventListener('click', () => updateNumber(1));
 const twoButton = document.querySelector('#two');
-twoButton.addEventListener('click', () => updateCount(2));
+twoButton.addEventListener('click', () => updateNumber(2));
 const threeButton = document.querySelector('#three');
-threeButton.addEventListener('click', () => updateCount(3));
+threeButton.addEventListener('click', () => updateNumber(3));
 const fourButton = document.querySelector('#four');
-fourButton.addEventListener('click', () => updateCount(4));
+fourButton.addEventListener('click', () => updateNumber(4));
 const fiveButton = document.querySelector('#five');
-fiveButton.addEventListener('click', () => updateCount(5));
+fiveButton.addEventListener('click', () => updateNumber(5));
 const sixButton = document.querySelector('#six');
-sixButton.addEventListener('click', () => updateCount(6));
+sixButton.addEventListener('click', () => updateNumber(6));
 const sevenButton = document.querySelector('#seven');
-sevenButton.addEventListener('click', () => updateCount(7));
+sevenButton.addEventListener('click', () => updateNumber(7));
 const eightButton = document.querySelector('#eight');
-eightButton.addEventListener('click', () => updateCount(8));
+eightButton.addEventListener('click', () => updateNumber(8));
 const nineButton = document.querySelector('#nine');
-nineButton.addEventListener('click', () => updateCount(9));
+nineButton.addEventListener('click', () => updateNumber(9));
 const zeroButton = document.querySelector('#zero');
-zeroButton.addEventListener('click', () => updateCount(0));
+zeroButton.addEventListener('click', () => updateNumber(0));
 
 const multiplyButton = document.querySelector('#multiply');
-multiplyButton.addEventListener('click', () => updateCount('&#215;'));
+multiplyButton.addEventListener('click', () => updateOperator('&#215;'));
 const divideButton = document.querySelector('#divide');
-divideButton.addEventListener('click', () => updateCount('&#247;'));
+divideButton.addEventListener('click', () => updateOperator('&#247;'));
 const plusButton = document.querySelector('#plus');
-plusButton.addEventListener('click', () => updateCount('&#43;'));
+plusButton.addEventListener('click', () => updateOperator('&#43;'));
 const minusButton = document.querySelector('#minus');
-minusButton.addEventListener('click', () => updateCount('&#8722;'));
+minusButton.addEventListener('click', () => updateOperator('&#8722;'));
 const decimalButton = document.querySelector('#decimal');
 decimalButton.addEventListener('click', () => updateCount('.'));
 
@@ -60,7 +108,7 @@ backButton.addEventListener('click', () => backSpace());
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', () => clearAll());
 const equalsButton = document.querySelector('#equals');
-equalsButton.addEventListener('click', () => console.log('Equals'));
+equalsButton.addEventListener('click', () => equals());
 
 
 function add(a, b) {
@@ -76,7 +124,7 @@ function divide(a, b) {
     return (a / b);
 }
 
-function operate(a, operator, b) {
+function calculate(a, operator, b) {
     switch(operator) {
         case "&#43;":
             return add(a, b);
